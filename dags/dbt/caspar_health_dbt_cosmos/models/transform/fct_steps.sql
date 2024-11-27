e6cdbc7e2df90998b8c45765283d369702fe6719
Jon Fernandez <jon.fernandez@friday.de>
2024-11-27 11:58:01 +0100

    Update: code structure and requirement management

---
+++ dags/dbt/caspar_health_dbt_cosmos/models/transform/fct_steps.sql
@@ -0,0 +1,8 @@
+SELECT
+    id,
+    external_id AS patient_id,
+    steps,
+    steps * 0.002 AS minutes,
+    submission_time::TIMESTAMP_TZ AS submitted_at,
+    updated_at::TIMESTAMP_TZ AS updated_at
+FROM {{ ref('stg_steps') }}
